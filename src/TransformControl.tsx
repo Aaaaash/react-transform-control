8c1d508423d15c08d0d00775b779b63351d6239d
sakuraash <841105127@qq.com>
2018-03-06 11:49:20 +0800

    update config, add storybook

---
+++ src/TransformControl.tsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+interface IProps { 
+}
+
+interface IState {
+}
+
+class TransformControl extends PureComponent<IProps, IState> {
+  render() {
+    return <div>hello world.</div>
+  }
+}
+
+export default TransformControl;
---
+++ stories/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import TransformControl from '../src/TransformControl';
+// const TransformControl = impo('../dist/TransformControl');
+// import TransformControl from '../dist/TransformControl';
+
+storiesOf('Welcome', module).add('to Storybook', () => <TransformControl />);
